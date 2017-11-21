554b71a0d652b33b744b198ba2d32e2c667c7b07
biphobe <hi@bipho.be>
2017-11-21 18:08:51 +0100

    Initial commit

---
+++ fixup.sh
@@ -0,0 +1,28 @@
+COMMITS=$(git log --oneline -9)
+
+function fix { 
+  COMMIT=$(echo "$COMMITS" | sed -n "$1"p)
+  HASH=$(echo $COMMIT | cut -d ' ' -f 1)
+
+  printf "\n\n"
+
+  git add .;
+  git commit -a --fixup=$HASH
+
+  EDITOR=true git rebase -i --autosquash HEAD~4
+}
+
+printf "Pick commit to fix:\n\n"
+
+while ((i++)); read -r line
+do
+  echo $i. $line;
+done <<< "$COMMITS"
+
+printf "\n"
+
+read -p "Well?" -n 1 -r
+case $REPLY in
+  *) fix $REPLY;;
+esac
+
